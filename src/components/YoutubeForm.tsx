f3e29cf3b4e7b12890f9df546165bc2fe49c9b04
Rhythm-08 <rhythmsharma808@gmail.com>
2024-03-03 19:05:52 +0530

    feat: understanding form validations in react forms

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,43 +1,78 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
-let renderCount = 0;       
+let renderCount = 0;
 type FormValues = {
-    username:string,
-    email:string,
-    channel:string
-}
+    username: string,
+    email: string,
+    channel: string
+};
+
 
 
 const YoutubeForm = () => {
     const form = useForm<FormValues>();
-    const { register,control, handleSubmit } = form;
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
     renderCount++;
 
-    const onSubmit = (data:FormValues) => {
-        console.log('Form Submitted',data);
-        
+    const onSubmit = (data: FormValues) => {
+        console.log('Form Submitted', data);
+
     }
 
-    return ( 
+    return (
         <div className="youtube_form">
-            <h1>YouTube Form {renderCount/2 }</h1>
-            <form onSubmit={handleSubmit(onSubmit)} >
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register("username")}/>
+            <h1>YouTube Form {renderCount / 2}</h1>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate >
+                <div className="formControl">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" {...register("username", {
+                        required: {
+                            value: true,
+                            message: "UserName is Required"
+                        }
+                    })} />
+                    <p className="error">{errors?.username?.message}</p>
+                </div>
 
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" {...register("email")} />
+                <div className="formControl">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" {...register("email", {
+                        pattern: {
+                            value: /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/,
+                            message: "Invalid Email Format"
+                        },
+                        required: {
+                            value: true,
+                            message: 'Email is Required'
+                        },
+                        validate: {
+                            notAdmin :(fieldValue) => {
+                                return fieldValue !== 'admin@example.com' || 'Enter a different email address';
+                            },
+                            notBlackListed : (fieldValue) => {
+                                return !fieldValue.endsWith('badDomain.com') || 'This domain is not supported';
+                            }
+                        }
+                    })} />
+                    <p className="error">{errors?.email?.message}</p>
+                </div>
 
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel")} />
+                <div className="formControl">
+                    <label htmlFor="channel">Channel</label>
+                    <input type="text" id="channel" {...register("channel", {
+                        required: { value: true, message: "Channel is Required" }
+                    })} />
+                    <p className="error">{errors?.channel?.message}</p>
+                </div>
 
                 <button>submit </button>
 
             </form>
-            <DevTool control = {control}/>
+            <DevTool control={control} />
         </div>
-     );
+    );
 }
- 
+
 export default YoutubeForm;
